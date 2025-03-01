7c794737d6603aba40573f5783c12e3ac1203e19
Usman Farooq <usman@uslc.online>
2025-03-02 01:17:07 +0500

    Fix missing dependencies

--- app.py
+++ app.py
@@ -141,4 +141,4 @@ def session_test():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
